0f8c51ae9250b8fde20a66022174a963755e40c0
link <45172505+890mn@users.noreply.github.com>
2023-07-04 22:32:42 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,22 +1,52 @@
 #include <iostream>
+#include <filesystem>
+#include <vector>
+#include <string>
 #include <fstream>
-#include <algorithm>
-using namespace std;
 
-void ReadFile(){
-    ifstream fin;
-    fin.open("1684748905742_525.ndf", ios::in);
-    if(!fin.is_open()){
-        cout << "Open failed." <<endl;
-        return ;
+std::vector<std::string> filenames;
+
+void GetDir() {
+    std::string directoryPath = "I:\\study data\\C_code\\LuCBatch";  // 替换为你的目录路径
+    
+    for (const auto& entry : std::filesystem::directory_iterator(directoryPath)){
+        if (entry.is_regular_file() && entry.path().extension() == ".ndf") {  // 只处理常规文件
+            std::string filename = entry.path().filename().string();
+            filenames.push_back(filename);
+        }
     }
-    string buff;
-    while(getline(fin, buff)) cout << buff << endl;
-    fin.close();
+
+    // 打印文件名
+    for (const std::string& filename : filenames){
+        std::cout << filename << std::endl;
+    }
 }
 
+void BitDelete() {
+    std::ifstream fin;
+    fin.open(filenames, std::ios::binary);
+    if (!fin.is_open()) {
+        std::cout << "Open failed." << std::endl;
+        return;
+    }
+    std::ofstream tmp;
+    tmp.open("tmp" + filenames +".mp4", std::ios::binary);
+    if (!tmp.is_open()) {
+        std::cout << "Failed to create tmp.mp4" << std::endl;
+        fin.close();
+        return;
+    }
+    fin.seekg(2);
+    char c;
+    while (fin.get(c))
+        tmp.put(c);
+    fin.close();
+    tmp.close();
+}
 
-int main(){
-    ReadFile();
+int main() {
+    std::cout << "Current working directory: " << std::filesystem::current_path() << std::endl;
+    GetDir();
+    BitDelete();
     return 0;
 }
